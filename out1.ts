b56b2e6a7e04bb8752c7d3bac4c27cf7c9346fe5
Anas <158191488+Mdas109@users.noreply.github.com>
2024-08-11 06:49:54 -0700

    Add files via upload

--- out1.ts
+++ out1.ts
@@ -1,11 +1,7 @@
 #EXTM3U
-#EXT-X-VERSION:3
 #EXT-X-TARGETDURATION:10
-#EXT-X-MEDIA-SEQUENCE:1
-
-#EXTINF:20.0,
-http://tv.cloudcdn.me:80/live.ts?channelId=275&uid=9246&deviceMac=00:1A:79:1F:2E:5A
-#EXTINF:20.0,
-http://tv.cloudcdn.me:80/live.ts?channelId=275&uid=9246&deviceMac=00:1A:79:1F:2E:5A
-#EXTINF:20.0,
-http://tv.cloudcdn.me:80/live.ts?channelId=275&uid=9246&deviceMac=00:1A:79:1F:2E:5A
+#EXT-X-VERSION:3
+#EXTINF:10, BANDWIDTH=3000000, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
+http://proxym3u.watchtv.best:80/play/live.php?mac=00:1A:79:64:DD:34&stream=155873&extension=ts
+#EXTINF:10, BANDWIDTH=3000000, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
+http://proxym3u.watchtv.best:80/play/live.php?mac=00:1A:79:64:DD:34&stream=155873&extension=ts
